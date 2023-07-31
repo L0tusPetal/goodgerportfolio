2e9f3dda61cf9f757012c083b4e27178686f81b6
Eve Frances <evefrances92@icloud.com>
2023-07-31 12:16:22 -0400

    help

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
-import headshot from '/Users/wren/Desktop/webprojects/Javascript/goodgerportfolio/goodgerportfolio/src/assets/png/Headshot.jpg'
+// import headshot from '/Users/wren/Desktop/webprojects/Javascript/goodgerportfolio/goodgerportfolio/src/assets/png/Headshot.jpg'
 
 export const headerData = {
     name: 'Chanté Goodger',
     title: "Production Manager & Writer",
     desciption:"I'm baby lumbersexual church-key slow-carb next level vape, listicle celiac. Raclette ramps chia normcore hell of. Lumbersexual shaman fanny pack pop-up, typewriter umami drinking vinegar. Yr gochujang vinyl narwhal 90's cliche shabby chic kombucha crucifix deep v chillwave sus seitan. Dummy text? More like dummy thicc text, amirite?",
-    image: headshot,
+    image: '',
     resumePdf: resume
 }
